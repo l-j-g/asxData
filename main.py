19c677391b0e2e5479fef2b0bfa3a6fdd41dfb75
l-j-g <lachlang93@gmail.com>
2021-12-17 23:40:35 +1100

    scaling up to 100 tickers

--- main.py
+++ main.py
@@ -11,32 +11,34 @@ with open('ASX_Listed_Companies_04-11-2021_04-29-57_AEDT.csv', newline='') as cs
 for index, ticker in enumerate(data):
     tickers.append(f"{ticker[0]}.AX")
 
-    
-for ticker in tickers[1:5]:
-
+count = 0    
+for ticker in tickers[1:100]:
+    count += 1
     filename = f"./data/{ticker}/"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
+    try:
+        info = si.get_company_info(ticker)
+        info['ticker'] = ticker
+        info.to_csv(f"./data/{ticker}/{ticker}_info.csv",)
+    except:
+        continue
 
-    info = si.get_company_info(ticker)
-    info['ticker'] = ticker
-    info.to_csv(f"./data/{ticker}/{ticker}_info.csv",)
+    #valuation = si.get_stats_valuation(ticker)
+    #valuation['ticker'] = ticker
+    #valuation.to_csv(f"./data/{ticker}/{ticker}_valuation.csv")
 
-    valuation = si.get_stats_valuation(ticker)
-    valuation['ticker'] = ticker
-    valuation.to_csv(f"./data/{ticker}/{ticker}_valuation.csv")
+    #income_statement = si.get_income_statement(ticker)
+    #income_statement['ticker'] = ticker
+    #income_statement.to_csv(f"./data/{ticker}/{ticker}_income_statement.csv")
 
-    income_statement = si.get_income_statement(ticker)
-    income_statement['ticker'] = ticker
-    income_statement.to_csv(f"./data/{ticker}/{ticker}_income_statement.csv")
+    #balance_sheet = si.get_balance_sheet(ticker)
+    #balance_sheet['ticker'] = ticker
+    #balance_sheet.to_csv(f"./data/{ticker}/{ticker}_balance_sheet.csv")
 
-    balance_sheet = si.get_balance_sheet(ticker)
-    balance_sheet['ticker'] = ticker
-    balance_sheet.to_csv(f"./data/{ticker}/{ticker}_balance_sheet.csv")
-
-    cash_flow = si.get_cash_flow(ticker)
-    cash_flow['ticker'] = ticker
-    cash_flow.to_csv(f"./data/{ticker}/{ticker}_cash_flow.csv")
-
+    #cash_flow = si.get_cash_flow(ticker)
+    #cash_flow['ticker'] = ticker
+    #cash_flow.to_csv(f"./data/{ticker}/{ticker}_cash_flow.csv")
+    print(count)
 
 
 
